7d29397bba86cd1ffe829ebb33b2e3c4d340ac91
Edil-Ken <edikoskyrim@gmail.com>
2023-07-11 08:42:10 +0600

    First JS

---
+++ script.js
@@ -0,0 +1,24 @@
+// let a = 4
+// let b = 55
+//
+// console.log(a + b, a * b, b / a, b % a)
+//
+// let a = 'Hello'
+// let b = 'Edil'
+//
+// console.log(a + ' ' + b)
+//
+// let a = 'twelve'
+// let b = 12
+//
+// console.log(a * b)
+//
+// let a = 5
+// let b = '5'
+//
+// console.log(a + +b, a + Number(b))
+//
+// let c = 2
+// let b = undefined
+//
+// console.log(c, b)
